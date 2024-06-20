e29e970e919db4d63463bbeb17e9b3f8dc4b4a6d
Ángel Vitores <ajvitores@gmail.com>
2024-06-20 18:48:36 +0200

    Enlace a página de New Balance

--- resources/views/emails/preinsConf.blade.php
+++ resources/views/emails/preinsConf.blade.php
@@ -11,11 +11,10 @@
 
 
     <p align="justify">Le recordamos que, si eligió pagar en la <strong>modalidad Club</strong>, puede abonar con pago en metálico o tarjeta bancaria en la oficina, o a través de transferencia bancaria en la cuenta de La Caixa con IBAN ES33 2100 4332 5801 0024 1501, y con la/s cuota/s como eligió.</p>
-    <p align="justify">Si eligió la modalidad Web New Balance, recordamos que el link para el pago del “pack de ropa + cuota” es el siguiente:</p>
-    <a href="http://enlace-nb.com">Enlace a Tienda de New Balance</a>
-    <p align="justify">Una vez que el pago sea verificado enviaremos a la dirección de correo indicada en el formulario el documento acreditativo de la inscripción así como el resguardo para la prueba de la ropa de deporte del club, en el caso de que sea necesario.</p>
+    <p align="justify">Si eligió la <b>modalidad Web New Balance</b>, recordamos que el link para el pago del “pack de ropa + cuota” es el siguiente:</p>
+    <a href="https://cdblaguna.newbalanceteam.club/">Enlace a Tienda de New Balance</a>
 
-    <p align="justify">Para los que eligieron la modalidad Club para el abono de la cuota, también tienen que acceder al anterior link de New Balance para la compra del “pack de ropa”, y seleccionar esa opción de compra.</p>
+    <p align="justify">Para los que eligieron la <b>modalidad Club</b> para el abono de la cuota, también tienen que acceder al anterior link de New Balance para la compra del “pack de ropa”, y seleccionar esa opción de compra.</p>
     <p align="justify">Os recordamos que también podéis comprar prendas sueltas en esta página web.</p>
     <p align="justify">Una vez que el pago sea verificado, enviaremos a la dirección de correo indicada en el formulario, el documento acreditativo de la inscripción.</p>
 </body>
